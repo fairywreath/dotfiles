fbbadfeee30e921f5bda5257e297a20fbf1a27b2
Darren Wihandi <dwihandi@uwaterloo.ca>
2022-06-26 14:15:25 -0400

    updated setup vim script

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ cp .tmux.conf $HOME/.tmux.conf
 tmux source-file $HOME/.tmux.conf
 # tmux source-file $HOME/.tmux-themepack/powerline/double/purple.tmuxtheme # yucks...
 
-
+# symlink...
+cp vim/colors/* ~/.vim/colors/
 cp .vimrc $HOME/.vimrc

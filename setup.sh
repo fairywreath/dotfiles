fe5d72ea50be31e9f66711c7066d7ecb6eb0770b
Darren Wihandi <nerradfour@gmail.com>
2023-05-19 20:28:57 -0400

    Added i3, picom and kitty configs. Removed nvim

--- setup.sh
+++ setup.sh
@@ -1,15 +1,10 @@
 wd=$(pwd)
 
-# tmux
-# cp .tmux.conf $HOME/.tmux.conf
-ln -s -f $wd/tmux.conf $HOME/.tmux.conf
-# tmux source-file $HOME/.tmux.conf
-# tmux source-file $HOME/.tmux-themepack/powerline/double/purple.tmuxtheme # yucks...
+ln -s $wd/tmux.conf $HOME/.tmux.conf
+ln -s $wd/vim/colors $HOME/.vim/colors
+ln -s $wd/vimrc $HOME/.vimrc
+ln -s $wd/i3 $HOME/.config/i3
+ln -s $wd/kitty $HOME/.config/kitty
+ln -s $wd/picom $HOME/.config/picom
 
-# vim
-ln -s -f $wd/vim/colors $HOME/.vim/colors
-ln -s -f $wd/vimrc $HOME/.vimrc
-
-# nvim
-ln -s -f $wd/nvim $HOME/.config/nvim
 
